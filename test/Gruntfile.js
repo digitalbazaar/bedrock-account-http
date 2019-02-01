4fb23a22d346f03a0afb18a41ba573f0d0f0a7f9
Matthew Collier <collier.matthew@gmail.com>
2019-02-01 11:50:24 -0500

    Implement test framework.

---
+++ test/Gruntfile.js
@@ -0,0 +1,18 @@
+/*!
+ * Copyright (c) 2018 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    shell: {
+      target: 'npm test'
+    },
+    watch: {
+      files: ['**/*', '!node_modules/**/*'],
+      tasks: ['shell']
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-shell');
+};
