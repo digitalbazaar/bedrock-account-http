5dd7b7143aaeb63f635ff2bb3e963f451b407729
andrewjones <ajones@digitalbazaar.com>
2019-02-02 13:11:48 -0600

    Add a test for get / with no email.

--- test/mocha/10-api.js
+++ test/mocha/10-api.js
@@ -5,6 +5,7 @@
 
 const bedrock = require('bedrock');
 const {config} = bedrock;
+const {expect} = chai;
 // apisauce is a wrapper around axios that provides improved error handling
 const {create} = require('apisauce');
 const https = require('https');
@@ -15,9 +16,14 @@ const api = create({
   httpsAgent: new https.Agent({rejectUnauthorized: false})
 });
 
-describe('bedrock-account-http', function bedrockAccountHttp() {
-  it('works great', async function worksGreat() {
+describe('bedrock-account-http  -- get /', function bedrockAccountHttp() {
+  it('should return 404 is no email is specified', async function worksGreat() {
     const result = await api.get('/');
-    console.log('RRRRRRRR', result);
+    const {status, data} = result;
+    expect(status, 'Expected 404 not found').to.equal(404);
+    expect(data).to.be.an('object');
+    const {message, type} = data;
+    expect(message).to.match(/account does not exist/i);
+    expect(type).to.match(/NotFoundError/i);
   });
 });
