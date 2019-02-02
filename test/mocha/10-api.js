573b8f3b05e71be49abbea583a1f19f35a24c969
andrewjones <ajones@digitalbazaar.com>
2019-02-02 14:22:48 -0600

    Add 2 more tests to get / route.

--- test/mocha/10-api.js
+++ test/mocha/10-api.js
@@ -9,15 +9,26 @@ const {config} = bedrock;
 const {create} = require('apisauce');
 const https = require('https');
 const brAccount = require('bedrock-account');
+const helpers = require('../helpers');
+const mockData = require('../mock.data');
+
+let actors;
+let accounts;
+let api;
+
 const baseURL =
  `https://${config.server.host}${config['account-http'].routes.basePath}`;
-const api = create({
-  baseURL,
-  httpsAgent: new https.Agent({rejectUnauthorized: false})
-});
-
+ 
 describe('bedrock-account-http  -- get /', function bedrockAccountHttp() {
-
+    before(async () => {
+    await helpers.prepareDatabase(mockData);
+    actors = await helpers.getActors(mockData);
+    accounts = mockData.accounts;
+    api = create({
+      baseURL,
+      httpsAgent: new https.Agent({rejectUnauthorized: false})
+    });
+  });
   it('should return 404 is no email is specified', async function worksGreat() {
     const result = await api.get('/');
     const {status, data} = result;
@@ -28,8 +39,22 @@ describe('bedrock-account-http  -- get /', function bedrockAccountHttp() {
     type.should.match(/NotFoundError/i);
   });
 
-  it.skip('return an account if the email is found', async function returnAccount(){
-    const accounts = await brAccount.getAll();
-    console.log('accounts', accounts); 
+  it('return 200 if the email is found', async function returnAccount(){
+    const email = 'alpha@example.com';
+    const result = await api.get('/', {exists: true, email});
+    const {status} = result;
+    should.equal(status, 200);
   });
+
+  it('return 404 if the email is not found', async function returnAccount(){
+    const email = 'not-found@example.com';
+    const result = await api.get('/', {exists: true, email});
+    const {status, data} = result;
+    should.equal(status, 404);
+    data.should.be.an('object');
+    const {message, type} = data;
+    message.should.match(/account does not exist/i);
+    type.should.match(/NotFoundError/i);
+  });
+
 });
