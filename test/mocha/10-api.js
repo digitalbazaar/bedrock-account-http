22baba85b19d0cd0fea9c2028fcec1f0dd24e336
andrewjones <ajones@digitalbazaar.com>
2019-02-03 16:17:15 -0600

    Add 3 tests for /:account.

--- test/mocha/10-api.js
+++ test/mocha/10-api.js
@@ -8,31 +8,28 @@ const {config} = bedrock;
 // apisauce is a wrapper around axios that provides improved error handling
 const {create} = require('apisauce');
 const https = require('https');
-const brAccount = require('bedrock-account');
 const helpers = require('../helpers');
 const mockData = require('../mock.data');
 
-let actors;
 let accounts;
 let api;
-let stub;
 
 const baseURL =
  `https://${config.server.host}${config['account-http'].routes.basePath}`;
- 
+
 describe('bedrock-account-http', function bedrockAccountHttp() {
-    before(async function setup() {
-      passportStub.callsFake((req, res, next) => next());
-      await helpers.prepareDatabase(mockData);
-      actors = await helpers.getActors(mockData);
-      accounts = mockData.accounts;
-      api = create({
-        baseURL,
-        httpsAgent: new https.Agent({rejectUnauthorized: false})
-        });
+  before(async function setup() {
+    passportStub.callsFake((req, res, next) => next());
+    await helpers.prepareDatabase(mockData);
+    await helpers.getActors(mockData);
+    accounts = mockData.accounts;
+    api = create({
+      baseURL,
+      httpsAgent: new https.Agent({rejectUnauthorized: false})
     });
+  });
   describe('get /', function getIndex() {
-    it('should return 404 is no email is specified', async function worksGreat() {
+    it('should return 404 with no email', async function worksGreat() {
       const result = await api.get('/');
       const {status, data} = result;
       should.equal(status, 404);
@@ -42,14 +39,14 @@ describe('bedrock-account-http', function bedrockAccountHttp() {
       type.should.match(/NotFoundError/i);
     });
 
-    it('return 200 if the email is found', async function returnAccount(){
+    it('return 200 if the email is found', async function returnAccount() {
       const email = 'alpha@example.com';
       const result = await api.get('/', {exists: true, email});
       const {status} = result;
       should.equal(status, 200);
     });
 
-    it('return 404 if the email is not found', async function returnAccount(){
+    it('return 404 if the email is not found', async function returnAccount() {
       const email = 'not-found@example.com';
       const result = await api.get('/', {exists: true, email});
       const {status, data} = result;
@@ -66,26 +63,63 @@ describe('bedrock-account-http', function bedrockAccountHttp() {
       const result = await api.post('/');
       result.status.should.equal(400);
     });
- 
+
     it('should return 201 if there is an email', async function() {
       const result = await api.post('/', {email: 'newuser@digitalbazaar.com'});
       result.status.should.equal(201);
-    });   
-  });  
+    });
+  });
 
   describe('get /:account', function() {
     it('should return an account', async function() {
       const {account: {id}} = accounts['alpha@example.com'];
       const {account: actor} = accounts['admin@example.com'];
+      actor["ACCOUNT_ACCESS"] = true;
+      delete actor.id;
       passportStub.callsFake((req, res, next) => {
-        req.user = {actor}; 
+        req.user = {actor};
         next();
       });
       const result = await api.get(`/${id}`);
+      result.status.should.equal(200);
+      const {data} = result;
+      data.should.be.an('object');
+      data.should.have.property('meta');
+      data.should.have.property('account');
     });
 
-    it.skip('should return 404', async function() {
+    it('should return 403', async function() {
+      const {account: {id}} = accounts['alpha@example.com'];
+      const {account: actor} = accounts['admin@example.com'];
+      actor["ACCOUNT_ACCESS"] = false;
+      delete actor.id;
+      passportStub.callsFake((req, res, next) => {
+        req.user = {actor};
+        next();
+      });
+      const result = await api.get(`/${id}`);
+      result.status.should.equal(403);
+      const {data} = result;
+      data.should.be.an('object');
+      data.should.not.have.property('meta');
+      data.should.not.have.property('account');
+    });
 
+    it('should return 404', async function() {
+      const id = "does-not-exist";
+      const {account: actor} = accounts['admin@example.com'];
+      actor["ACCOUNT_ACCESS"] = true;
+      delete actor.id;
+      passportStub.callsFake((req, res, next) => {
+        req.user = {actor};
+        next();
+      });
+      const result = await api.get(`/${id}`);
+      result.status.should.equal(404);
+      const {data} = result;
+      data.should.be.an('object');
+      data.should.not.have.property('meta');
+      data.should.not.have.property('account');
     });
   });
 
