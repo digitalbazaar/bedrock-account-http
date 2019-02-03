63c3c4a1c55ae307e2da7d5f93c4739d252b72f8
andrewjones <ajones@digitalbazaar.com>
2019-02-02 19:07:15 -0600

    Add 2 tests for post /.
    one test is for if there is no email in the account creation
    right now this succeeds when it should not so I am skipping it.

--- test/mocha/10-api.js
+++ test/mocha/10-api.js
@@ -19,8 +19,8 @@ let api;
 const baseURL =
  `https://${config.server.host}${config['account-http'].routes.basePath}`;
  
-describe('bedrock-account-http  -- get /', function bedrockAccountHttp() {
-    before(async () => {
+describe('bedrock-account-http', function bedrockAccountHttp() {
+    before(async function setup() {
     await helpers.prepareDatabase(mockData);
     actors = await helpers.getActors(mockData);
     accounts = mockData.accounts;
@@ -29,32 +29,47 @@ describe('bedrock-account-http  -- get /', function bedrockAccountHttp() {
       httpsAgent: new https.Agent({rejectUnauthorized: false})
     });
   });
-  it('should return 404 is no email is specified', async function worksGreat() {
-    const result = await api.get('/');
-    const {status, data} = result;
-    should.equal(status, 404);
-    data.should.be.an('object');
-    const {message, type} = data;
-    message.should.match(/account does not exist/i);
-    type.should.match(/NotFoundError/i);
+  describe('get /', function getIndex() {
+    it('should return 404 is no email is specified', async function worksGreat() {
+      const result = await api.get('/');
+      const {status, data} = result;
+      should.equal(status, 404);
+      data.should.be.an('object');
+      const {message, type} = data;
+      message.should.match(/account does not exist/i);
+      type.should.match(/NotFoundError/i);
+    });
+
+    it('return 200 if the email is found', async function returnAccount(){
+      const email = 'alpha@example.com';
+      const result = await api.get('/', {exists: true, email});
+      const {status} = result;
+      should.equal(status, 200);
+    });
+
+    it('return 404 if the email is not found', async function returnAccount(){
+      const email = 'not-found@example.com';
+      const result = await api.get('/', {exists: true, email});
+      const {status, data} = result;
+      should.equal(status, 404);
+      data.should.be.an('object');
+      const {message, type} = data;
+      message.should.match(/account does not exist/i);
+      type.should.match(/NotFoundError/i);
+    });
   });
 
-  it('return 200 if the email is found', async function returnAccount(){
-    const email = 'alpha@example.com';
-    const result = await api.get('/', {exists: true, email});
-    const {status} = result;
-    should.equal(status, 200);
-  });
-
-  it('return 404 if the email is not found', async function returnAccount(){
-    const email = 'not-found@example.com';
-    const result = await api.get('/', {exists: true, email});
-    const {status, data} = result;
-    should.equal(status, 404);
-    data.should.be.an('object');
-    const {message, type} = data;
-    message.should.match(/account does not exist/i);
-    type.should.match(/NotFoundError/i);
-  });
+  describe('post /', function() {
+    it.skip('should return 400 if there is no email', async function() {
+      const result = await api.post('/');
+      result.status.should.equal(400);
+    });
+ 
+    it('should return 201 if there is an email', async function() {
+      const result = await api.post('/', {email: 'newuser@digitalbazaar.com'});
+      result.status.should.equal(201);
+    });
+   
+  });  
 
 });
