483b625a46341b0b7d7334b5c93e74ce3e0f16cb
andrewjones <ajones@digitalbazaar.com>
2019-02-07 12:59:45 -0600

    Change status validator to an enum.

--- schemas/account.js
+++ schemas/account.js
@@ -31,9 +31,7 @@ const setStatus = {
   properties: {
     status: {
       type: 'string',
-      pattern: '^(active|deleted)$',
-      minLength: 6,
-      maxLength: 7
+      enum: ['active', 'deleted']
     }
   }
 };
