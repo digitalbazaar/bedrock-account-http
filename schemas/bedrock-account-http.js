eaeacd9e41ca8d7c32eb92d058e7c627f89cf749
Dave Longley <dlongley@digitalbazaar.com>
2022-03-23 17:26:16 -0400

    Use `esm.js` to transpile internals from ESM to CommonJS.

--- schemas/bedrock-account-http.js
+++ schemas/bedrock-account-http.js
@@ -1,65 +1,71 @@
 /*!
  * Copyright (c) 2019-2022 Digital Bazaar, Inc. All rights reserved.
  */
-'use strict';
+import {schemas} from 'bedrock-validation';
 
-const {schemas} = require('bedrock-validation');
-
-exports.create = () => ({
-  title: 'bedrock-accounts-http account creation post',
-  type: 'object',
-  required: ['email'],
-  additionalProperties: false,
-  properties: {
-    email: schemas.email()
-  }
-});
-
-exports.get = () => ({
-  title: 'bedrock-accounts-http get',
-  type: 'object',
-  required: ['email'],
-  additionalProperties: false,
-  properties: {
-    email: schemas.email(),
-    exists: {
-      type: 'boolean'
-    },
-    after: {
-      type: 'string',
-      minLength: 0
-    },
-    limit: {
-      type: 'integer',
-      minimum: 1,
-      maximum: 1000
+export function create() {
+  return {
+    title: 'bedrock-accounts-http account creation post',
+    type: 'object',
+    required: ['email'],
+    additionalProperties: false,
+    properties: {
+      email: schemas.email()
     }
-  }
-});
+  };
+}
 
-exports.update = () => ({
-  title: 'bedrock-accounts-http account update',
-  required: ['patch', 'sequence'],
-  type: 'object',
-  additionalProperties: false,
-  properties: {
-    patch: schemas.jsonPatch(),
-    sequence: {
-      type: 'integer',
-      minimum: 0
+export function get() {
+  return {
+    title: 'bedrock-accounts-http get',
+    type: 'object',
+    required: ['email'],
+    additionalProperties: false,
+    properties: {
+      email: schemas.email(),
+      exists: {
+        type: 'boolean'
+      },
+      after: {
+        type: 'string',
+        minLength: 0
+      },
+      limit: {
+        type: 'integer',
+        minimum: 1,
+        maximum: 1000
+      }
     }
-  }
-});
+  };
+}
 
-exports.setStatus = () => ({
-  title: 'bedrock-accounts-http patch set status',
-  required: ['status'],
-  type: 'object',
-  additionalProperties: false,
-  properties: {
-    status: {
-      type: 'string',
-      enum: ['active', 'disabled', 'deleted']
+export function update() {
+  return {
+    title: 'bedrock-accounts-http account update',
+    required: ['patch', 'sequence'],
+    type: 'object',
+    additionalProperties: false,
+    properties: {
+      patch: schemas.jsonPatch(),
+      sequence: {
+        type: 'integer',
+        minimum: 0
+      }
     }
-  }
-});
+  };
+}
+
+export function setStatus() {
+  return {
+    title: 'bedrock-accounts-http patch set status',
+    required: ['status'],
+    type: 'object',
+    additionalProperties: false,
+    properties: {
+      status: {
+        type: 'string',
+        enum: ['active', 'disabled', 'deleted']
+      }
+    }
+  };
+}
