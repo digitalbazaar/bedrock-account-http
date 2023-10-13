dc1981819fca135d447333bf43ab49493053be11
Mandy Venables <mandylvenables@gmail.com>
2023-10-13 11:40:29 -0400

    Add tests.

--- test/mocha/10-api.js
+++ test/mocha/10-api.js
@@ -90,6 +90,17 @@ describe('bedrock-account-http', function bedrockAccountHttp() {
   });
 
   describe('post /', function() {
+    it('should create account with authorization', async function() {
+      const email = {email: 'auth@digitalbazaar.com', authorization};
+      const result = await api.post('/', email);
+      result.status.should.equal(201);
+    });
+    it('should create account without authorization', async function() {
+      config['account-http'].registration.authorizationRequired = false;
+      const email = {email: 'noauth@digitalbazaar.com'};
+      const result = await api.post('/', email);
+      result.status.should.equal(201);
+    });
     it('should return 400 if there is no email', async function() {
       const result = await api.post('/');
       validationError(result, 'Create Account', /email/i);
